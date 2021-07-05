231e575301b982a20f6064c5255d958c78678216
junboXiang <junboxiang@126.com>
2021-07-05 19:41:20 +0800

    orderState 调整

--- oms-common/src/main/java/org/arch/oms/common/enums/OrderState.java
+++ oms-common/src/main/java/org/arch/oms/common/enums/OrderState.java
@@ -12,14 +12,17 @@ import java.util.Objects;
  */
 public enum OrderState implements ValueDescription<Integer> {
 
+    /*
+     每个状态中间间隔20  方便后期中间加状态
+     */
     NONE(0, "无"),
-    WAITING_PAYMENT(10, "等待支付"),
-    PAYMENT_SUCCESS(20, "支付成功"),
-    SHIPPING(30, "等待收货"),
-    WAITING_COMMENT(40, "售后"),
-    AFTER_SALE(50, "售后"),
-    COMPLETE(60, "已完成"),
-    CANCELLED(70, "已取消"),
+    WAITING_PAYMENT(20, "等待支付"),
+    PAYMENT_SUCCESS(40, "支付成功"),
+    SHIPPING(60, "等待收货"),
+    WAITING_COMMENT(80, "等待评价"),
+    AFTER_SALE(100, "售后"),
+    COMPLETE(120, "已完成"),
+    CANCELLED(140, "已取消"),
     ;
 
     private final int value;
